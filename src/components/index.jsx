3631f22fdd67c5200087d5086ba9b3df907047ba
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-01 19:31:00 +0430

    create components

---
+++ src/components/index.jsx
@@ -0,0 +1,18 @@
+export { default as Button} from "./button/Button"
+export { default as ThemeSettings} from "./themeSettings/ThemeSettings"
+export { default as Sidebar} from "./sidebar/Sidebar"
+
+
+export { default as Navbar} from "./navbar/Navbar"
+export { default as Footer} from "./footer/Footer"
+export { default as Chat} from "./chat/Chat"
+export { default as Cart} from "./cart/Cart"
+export { default as Notification} from "./notification/Notification"
+export { default as UserProfile} from "./userProfile/UserProfile"
+
+export { default as SparkLine} from "./charts/sparkLine"
+export { default as LineChart} from "./charts/lineChart"
+export { default as Stacked} from "./charts/stacked"
+export { default as Pie} from "./charts/pie"
+export { default as ChartsHeader} from "./chartsHeader/ChartsHeader"
+export { default as Header} from "./header/Header"
