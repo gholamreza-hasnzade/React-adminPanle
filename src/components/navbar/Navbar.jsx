05e8c55b94d591dc868a9bbeba8455684b9d4c5a
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-04 11:59:57 +0430

    [hotfixdd] : navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,122 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+import avator from "../../data/avatar.jpg";
+
+import { Cart, Chat, Notification, UserProfile } from "../";
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const NavButton = ({ title, customFun, icon, color, dotColor }) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={customFun}
+      style={{ color }}
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+    >
+      <span
+        style={{ background: dotColor }}
+        className="absoute inline-flex rounded-full
+        h-2 w-2 right-2 top-2"
+      />
+        {icon}
+    </button>
+  </TooltipComponent>
+);
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const {
+    activeMenu,
+    setActiveMenu,
+    isClicked,
+    setIsClicked,
+    handleClick,
+    screenSize,
+    setScreenSize,
+  } = useStateContext();
 
-export default Navbar
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [setScreenSize]);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize, setActiveMenu]);
+
+  return (
+    <div
+      className="flex justify-between p-2 
+  md:mx-6 relative"
+    >
+      <NavButton
+        title="Menu"
+        customFun={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFun={() => handleClick("cart")}
+          dotColor="#03c9d7"
+          icon={<FiShoppingCart />}
+        />
+
+        <NavButton
+          title="Chat"
+          customFun={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+
+        <NavButton
+          title="Notification"
+          customFun={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray
+           rounded-full"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img className="rounded-full w-8 h-8 " src={avator} alt="avator" />
+            <p>
+              <span className="text-gray-400 text-14">Hi ,</span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Gholamreza
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+
+        {isClicked.cart && <Cart />}
+        {isClicked.chat && <Chat />}
+        {isClicked.notification && <Notification />}
+        {isClicked.userProfile && <UserProfile />}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
