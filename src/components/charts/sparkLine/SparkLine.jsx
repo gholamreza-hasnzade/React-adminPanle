c481cb86ba1de834b57bee7400b6fefa42755251
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-06-05 16:10:57 +0430

    [init data]

--- src/components/charts/sparkLine/SparkLine.jsx
+++ src/components/charts/sparkLine/SparkLine.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const SparkLine = () => {
+import {
+  SparklineComponent ,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
+const SparkLine = ({ id, type, height, width, data, color, currentColor }) => {
   return (
-    <div>SparkLine</div>
-  )
-}
+    <SparklineComponent 
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={type}
+    >
+      <Inject services={[SparklineComponent]}/>
+    </SparklineComponent>
+  );
+};
 
-export default SparkLine
+export default SparkLine;
--- src/pages/ecommerce/Ecommerce.jsx
+++ src/pages/ecommerce/Ecommerce.jsx
@@ -39,8 +39,11 @@ const Ecommerce = () => {
           </div>
         </div>
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-        {earningData.map((item) => (
-            <div key={item.title} className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl ">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white h-44 dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56  p-4 pt-9 rounded-2xl "
+            >
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
@@ -60,9 +63,62 @@ const Ecommerce = () => {
         </div>
       </div>
       <div className="flex gap-10 flex-wrap justify-center">
-        <div className="bg-white dark:bg-secondary-dark-bg
-        m-3 p-4 rounded-xl md:w-780">
-
+        <div
+          className="bg-white 
+          dark:text-gray-200 
+          dark:bg-secondary-dark-bg
+          m-3 p-4 rounded-2xl md:w-780"
+        >
+          <div className="flex justify-between">
+            <p className="font-semibold text-xl">Revenue Updates</p>
+            <div className="flex items-center gap-4">
+              <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Expense</span>
+              </p>
+              <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                <span>
+                  <GoPrimitiveDot />
+                </span>
+                <span>Budget</span>
+              </p>
+            </div>
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span
+                    className="p-1.5 hover:drop-shadow-xl 
+                  cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs"
+                  >
+                    23%
+                  </span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
+              <div className="mt-8">
+                <p>
+                  <span className="text-3xl font-semibold">$48,438</span>
+                </p>
+                <p className="text-gray-500 mt-1">Expense</p>
+              </div>
+              <div className="mt-5">
+                <SparkLine
+                  currentColor="blue"
+                  id="line-sparkline"
+                  type="Line"
+                  height="80px"
+                  width="250px"
+                  data={SparklineAreaData}
+                  color="blue"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
